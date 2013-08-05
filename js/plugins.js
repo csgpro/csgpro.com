fbd289617fa4ea4893ef6faf05bdb9b5ceb3f855
Jon de la Motte <jond@csgpro.com>
2013-08-05 16:49:12 -0700

    sticky nav working

--- js/plugins.js
+++ js/plugins.js
@@ -1,24 +1,46 @@
 // Avoid `console` errors in browsers that lack a console.
 (function() {
-    var method;
-    var noop = function () {};
-    var methods = [
-        'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
-        'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
-        'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
-        'timeStamp', 'trace', 'warn'
-    ];
-    var length = methods.length;
-    var console = (window.console = window.console || {});
+  var method;
+  var noop = function () {};
+  var methods = [
+  'assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error',
+  'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log',
+  'markTimeline', 'profile', 'profileEnd', 'table', 'time', 'timeEnd',
+  'timeStamp', 'trace', 'warn'
+  ];
+  var length = methods.length;
+  var console = (window.console = window.console || {});
 
-    while (length--) {
-        method = methods[length];
+  while (length--) {
+    method = methods[length];
 
         // Only stub undefined methods.
         if (!console[method]) {
-            console[method] = noop;
+          console[method] = noop;
         }
-    }
-}());
+      }
+    }());
 
-// Place any jQuery/helper plugins in here.
+////////////////////
+// Sticky Nav Bar //
+////////////////////
+(function(){
+  var isFixed = false
+    , nav = $('nav')
+    , navBottom = nav.length && nav.offset().top
+    , w = $(window)
+    , spacer = $('#nav-spacer');
+
+  $(window).on('scroll', function(){
+    if (w.scrollTop() >= navBottom && !isFixed){
+      isFixed = true;
+      nav.addClass('fixed-menu');
+      spacer.removeClass('hidden');
+    } else if (w.scrollTop() < navBottom && isFixed) {
+      isFixed = false;
+      nav.removeClass('fixed-menu');
+      spacer.addClass('hidden');
+    }
+  });
+
+}());
