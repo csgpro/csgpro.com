6d97520a2f33928e2a3d8199adfb9c03bca02b9b
Jon de la Motte <jond@csgpro.com>
2013-08-13 23:39:03 -0700

    added stylus and nib, refactoring

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
+  , stylus = require('stylus')
+  , nib = require('nib')
   , path = require('path');
 
 var app = express();
+
+function compile(str, path){
+  return stylus(str)
+    .set('filename', path)
+    .use(nib());
+}
 
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
+app.use(stylus.middleware({ // stylus CSS preprocessor
+  src: __dirname + '/public'
+, compile: compile
+}));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
@@ -28,7 +39,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
