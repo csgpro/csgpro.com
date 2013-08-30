a5667bcc3c0708cc29e8de4778d0decaa74f7607
Jon de la Motte <jond@csgpro.com>
2013-08-30 16:22:36 -0700

    fixing nav with ids instead of data-

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -182,7 +182,8 @@ function init(options){
     var me = $(this)
       , text = me.attr('id').match(/swapper-(\w+)/)[1]
       // TODO: replace the data-swipe selector with an ID selector. Much faster
-      , target = $('.swappable section[data-swap="' + text + '"]') // slow?
+      // , target = $('.swappable section[data-swap="' + text + '"]') // slow?
+      , target = $('#swappable-' + text)
       , brk = options.breakpoint
       , pageWidth = document.documentElement.clientWidth;
 
@@ -192,6 +193,7 @@ function init(options){
     if (pageWidth < brk) { // mobile
       var isAccordion = /accordion/.test(me.parent()[0].className);
       if (isAccordion) {
+        // WIP
         var contentElements = $('.swappable section[data-swap="' + text + '"] p')
           , titleElement = $('.swappable section[data-swap="' + text + '"] .title')
           , title = titleElement[0].innerText
