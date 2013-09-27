e359332f9606442c8ec026c69a90e277c3f823a4
Jon de la Motte <jond@csgpro.com>
2013-09-27 16:15:40 -0700

    added more copy

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -434,49 +434,56 @@ module.exports = init;
 
 'use strict';
 
-function init(options){
+var options;
+
+function init(o){
+  options = o;
+
   $('.swapper > li').click(function(){
-    var me = $(this)
-      , text = me.attr('id').match(/swapper-(\w+)/)[1]
-      // TODO: replace the data-swipe selector with an ID selector. Much faster
-      // , target = $('.swappable section[data-swap="' + text + '"]') // slow?
-      , target = $('#swappable-' + text)
-      , brk = options.breakpoint
-      , pageWidth = document.documentElement.clientWidth
-      , isAccordion = /accordion/.test(me.parent()[0].className);
-
-    document.greer = me; // DEBUG
-
-
-    if (pageWidth < brk) { // mobile
-      if (isAccordion) {
-        var content = $('#swappable-' + text);
-        var contentTarget = $('#swapper-target-' + text );
-
-        if (me.hasClass('active')) {
-          me.removeClass('active');
-          contentTarget.html('');
-        } else {
-          me.addClass('active');
-          contentTarget.html(content.children().clone());
-          contentTarget.children()[0].style['padding-top'] = '12px';
-        }
-
-      }
-      // something mobile
-    } else {               // desktop
-      me.addClass('active');
-      me.siblings().removeClass('active');
-      target.siblings().addClass('gone');
-      target.removeClass('gone');
-    }
+    swap($(this));
   });
+}
+
+function swap(element) {
+  var me = element
+    , text = me.attr('id').match(/swapper-(\w+)/)[1]
+
+    // TODO: replace the data-swipe selector with an ID selector. Much faster
+    // , target = $('.swappable section[data-swap="' + text + '"]') // slow?
+    , target = $('#swappable-' + text)
+    , brk = options.breakpoint
+    , pageWidth = document.documentElement.clientWidth
+    , isAccordion = /accordion/.test(me.parent()[0].className);
+
+  if (pageWidth < brk) { // mobile
+    if (isAccordion) {
+      var content = $('#swappable-' + text);
+      var contentTarget = $('#swapper-target-' + text );
+
+      if (me.hasClass('active')) {
+        me.removeClass('active');
+        contentTarget.html('');
+      } else {
+        me.addClass('active');
+        contentTarget.html(content.children().clone());
+        contentTarget.children()[0].style['padding-top'] = '12px';
+      }
+    }
+  } else {               // desktop
+    me.addClass('active');
+    me.siblings().removeClass('active');
+    target.siblings().addClass('gone');
+    target.removeClass('gone');
+  }
 }
 
 ////////////////////
 // MODULE EXPORTS //
 ////////////////////
 module.exports = init;
+// DANGER global scope
+window.swapElement = swap;
+
 
 },{}],9:[function(require,module,exports){
 /**
