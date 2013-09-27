e11da29dd5e4876c8dd347bcf511043f85a36782
Jon de la Motte <jond@csgpro.com>
2013-09-27 09:40:27 -0700

    added gzip

--- app.js
+++ app.js
@@ -98,6 +98,7 @@ app.set('port', process.env.PORT || 80);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.logger('dev'));
+app.use(express.compress());
 app.use(express.cookieParser());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
