f2ac14a51ca08214f502378d45df1da8593397a7
Ken Howard <kenh@csgpro.com>
2014-10-25 07:59:31 -0700

    Handling errors with JSON.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -24,7 +24,12 @@ self.getPosts = function (req, res) {
 	if(topicFilter) {
 		db.getFilteredCollection('allposts/?' + topicFilter, function(err, data) {
 			if (err) {
-				res.render('404');
+				var msg = {
+					status: 'fail',
+					message: 'Error Retrieving Posts',
+					error: err
+				};
+				res.send(JSON.stringify(msg));
 			} else {
 				res.send(data);
 			}
@@ -32,7 +37,12 @@ self.getPosts = function (req, res) {
 	} else {
 		db.getCollection('posts', function(err, data) {
 			if (err) {
-				res.render('404');
+				var msg = {
+					status: 'fail',
+					message: 'Error Retrieving Posts',
+					error: err
+				};
+				res.send(JSON.stringify(msg));
 			} else {
 				if (categoryFilter) {
 					// Do something with the data;
@@ -48,7 +58,12 @@ self.getPostByID = function (req, res) {
 	var id = req.params.id;
 	db.getItem('posts', id, function (err, data) {
 		if (err) {
-			res.render('404');
+			var msg = {
+				status: 'fail',
+				message: 'Error Retrieving Post',
+				error: err
+			};
+			res.send(JSON.stringify(msg));
 		} else {
 			res.send(data);
 		}
@@ -58,43 +73,55 @@ self.getPostByID = function (req, res) {
 self.createPost = function (req, res) {
 	db.createItem('posts', req.body, function (err, data) {
 		if (err) {
-			res.render('404');
+			var msg = {
+				status: 'fail',
+				message: 'Error Creating Post',
+				error: err
+			};
 		} else {
 			var msg = {
 				status: 'success',
 				message: 'Successfully Created Post',
 				id: data.id
 			};
-			res.send(JSON.stringify(msg));
 		}
+		res.send(JSON.stringify(msg));
 	});
 };
 
 self.updatePost = function (req, res) {
 	db.updateItem('posts', req.params.id, req.body, function (err, data) {
 		if (err) {
-			res.render('404');
+			var msg = {
+				status: 'fail',
+				message: 'Error Updating Post',
+				error: err
+			};
 		} else {
 			var msg = {
 				status: 'success',
 				message: 'Successfully Updated Post',
 				id: data.id
 			};
-			res.send(JSON.stringify(msg));
 		}
+		res.send(JSON.stringify(msg));
 	});
 };
 
 self.deletePost = function (req, res) {
 	db.deleteItem('posts', req.params.id, function (err, data) {
 		if (err) {
-			res.render('404');
+			var msg = {
+				status: 'fail',
+				message: 'Error Deleting Post',
+				error: err
+			};
 		} else {
 			var msg = {
 				status: 'success',
 				message: 'Successfully Deleted Post'
 			}
-			res.send(JSON.stringify(msg));
 		}
+		res.send(JSON.stringify(msg));
 	});
 };
