221203dc8f8315e00dc75f57a2f5bd23f0276388
Ken Howard <kenh@csgpro.com>
2014-10-25 07:58:56 -0700

    Added topics controller.

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,9 @@
 'use strict';
 
 var posts		= require('./api/posts'),
-    users       = require('./api/users');
+    users       = require('./api/users'),
+    topics      = require('./api/topics');
 
 module.exports.posts = posts;
 module.exports.users = users;
+module.exports.topics = topics;
