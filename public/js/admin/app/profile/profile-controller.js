479ac477b7b9c68a029a34cc6ca1f924c38864f9
Ken Howard <kenh@csgpro.com>
2014-10-26 08:42:32 -0700

    Fix CreateDate input. Added image url input field.

--- public/js/admin/app/profile/profile-controller.js
+++ public/js/admin/app/profile/profile-controller.js
@@ -2,11 +2,13 @@
 	'use strict';
 
 	angular.module('app')
-		.controller('ProfileCtrl', ['data', function(data) {
+		.controller('ProfileCtrl', ['data', '$filter', function(data, $filter) {
 			// Do Awesome Stuff!
 			var profileViewModel = this;
 
 			profileViewModel.user = data;
+
+			profileViewModel.user.CreateDateDisplay = $filter('date')(profileViewModel.user.CreateDate);
 
 		}]);
 })();
