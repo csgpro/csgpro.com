cbd5ff40cd73a8c75e3ff253acfb0b7d6c7f67cd
Ken Howard <kenh@csgpro.com>
2014-10-28 09:46:00 -0700

    Add file upload method.

--- public/js/admin/app/profile/profile-controller.js
+++ public/js/admin/app/profile/profile-controller.js
@@ -2,13 +2,27 @@
 	'use strict';
 
 	angular.module('app')
-		.controller('ProfileCtrl', ['data', '$filter', function(data, $filter) {
+		.controller('ProfileCtrl', ['data', '$filter', '$upload', function(data, $filter, $upload) {
 			// Do Awesome Stuff!
 			var profileViewModel = this;
 
 			profileViewModel.user = data;
 
 			profileViewModel.user.CreateDateDisplay = $filter('date')(profileViewModel.user.CreateDate);
+
+			profileViewModel.onFileSelect = function($files) {
+				for (var i = 0; i < $files.length; i++) {
+					var file = $files[i];
+					profileViewModel.upload = $upload.upload({
+						url: '/upload/img/author',
+						file: file
+					}).progress(function(evt) {
+						console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
+					}).success(function(data, status, headers, config) {
+						profileViewModel.user.ProfileUrl = data.url;
+					});
+				}
+			};
 
 		}]);
 })();
