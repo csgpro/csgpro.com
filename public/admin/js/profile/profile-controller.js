cd65acb38fdc0e4cac8af7cba7f7ac3f5d703c13
Ken Howard <kenh@csgpro.com>
2014-11-15 08:02:29 -0800

    Restring image upload to only one image.

--- public/admin/js/profile/profile-controller.js
+++ public/admin/js/profile/profile-controller.js
@@ -12,22 +12,17 @@
 				ProfileUrl: data.ProfileUrl,
 				TwitterHandle: data.TwitterHandle,
 				Username: data.Username
-			}
+			};
 
 			profileViewModel.user.CreateDateDisplay = $filter('date')(data.CreateDate);
 
-			profileViewModel.onFileSelect = function($files) {
-				for (var i = 0; i < $files.length; i++) {
-					var file = $files[i];
-					profileViewModel.upload = $upload.upload({
-						url: '/upload/img/author',
-						file: file
-					}).progress(function(evt) {
-						console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-					}).success(function(data, status, headers, config) {
-						profileViewModel.user.ProfileUrl = data.url;
-					});
-				}
+			profileViewModel.onFileSelect = function(file) {
+				profileViewModel.upload = $upload.upload({
+					url: '/upload/img/author',
+					file: file
+				}).success(function(data, status, headers, config) {
+					profileViewModel.user.ProfileUrl = data.url;
+				});
 			};
 
 			profileViewModel.canEdit = false;
