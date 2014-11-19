5dc6b5c4bde07773e45f7a8ad89c5fde86396a45
Ken Howard <kenh@csgpro.com>
2014-11-18 22:56:19 -0800

    Set up posts datatable.

--- public/admin/js/posts/posts-controller.js
+++ public/admin/js/posts/posts-controller.js
@@ -2,34 +2,36 @@
 	'use strict';
 
 	angular.module('app')
-		.controller('PostsCtrl', ['data', '$timeout', function(data, $timeout) {
+		.controller('PostsCtrl', ['common', 'data', function(common, data) {
 			// Do Awesome Stuff!
 			var postsViewModel = this;
 
 			postsViewModel.posts = data;
 
-			// postsViewModel.gridRowSelectAction = function(row) {
-			// 	alert('id: ' + row.entity.id);
-			// };
-			//
-			// var editCell = function () {
-			// 	return '<div class="grid-actions ui-grid-cell-contents"><button class="btn btn-xs btn-primary" ng-click="getExternalScopes().gridRowSelectAction(row)"><span class="glyphicon glyphicon-pencil"></span> Edit</button></div>';
-			// };
-			//
-			// postsViewModel.gridOptions = {
-			// 	data: postsViewModel.posts,
-			// 	columnDefs: [
-			// 		{ name: 'id', width: '70' },
-			// 		{ name: 'AuthorUsername', width: '100' },
-			// 		{ name: 'Title' },
-			// 		{ name: 'Topics', width: '100' },
-			// 		{ name: 'Category', width: '100' },
-			// 		{ name: 'PublishDate', width: '100' },
-			// 		{ name: 'UpdateDate', width: '100' },
-			// 		{ name: 'Actions', width: '100', cellTemplate: editCell() }
-			// 	],
-			// 	rowTemplate: 'partials/clickable-row.html'
-			// };
+			postsViewModel.CreateDateDisplay = common.convertToDate(data.CreateDate);
+			postsViewModel.PublishDateDisplay = common.convertToDate(data.PublishDate);
+
+			postsViewModel.goToPost = function (post) {
+				common.goToUrl('/posts/' + post.id);
+			};
+
+			var cellButtons = '<div class="ngCellText"><button class="btn btn-xs btn-primary" ng-click="$parent.postsViewModel.goToPost(row.entity)" tooltip="View Post" tooltip-placement="left"><span class="glyphicon glyphicon-search"></span></button></div>';
+
+			// Set up datatable
+		    postsViewModel.datatable = {
+		        data: 'postsViewModel.posts',
+		        enableRowSelection: false,
+		        enableHighlighting: true,
+		        columnDefs: [
+	                { field: 'AuthorFullName', displayName: 'Author', width: 100 },
+	                { field: 'Title', displayName: 'Title' },
+	                { field: 'Topics', displayName: 'Topics', width: 150 },
+					{ field: 'Category', displayName: 'Category', width: 100 },
+					{ field: 'CreateDate', displayName: 'Created', width: 100, cellFilter: 'date:"M/d/yyyy"' },
+					{ field: 'PublishDate', displayName: 'Published', width: 100, cellFilter: 'date:"M/d/yyyy"' },
+	                { width: 120, cellTemplate: cellButtons, cellClass: 'allowOverflow' }
+		        ]
+		    };
 
 		}]);
 })();
