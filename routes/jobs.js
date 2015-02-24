d5cdd2627913dd42d7290c09dc12620c7d2cdfbf
Ken Howard <ken@simplicatedweb.com>
2015-02-23 16:19:38 -0800

    Prevent HiringThing errors from bubbling up.

--- routes/jobs.js
+++ routes/jobs.js
@@ -24,15 +24,7 @@ self.getJobs = function (req, res) {
         }
     }
     ht.getJobs(filter, function (err, data) {
-        if (err) {
-            var msg = {
-                status: 'fail',
-                message: err.error,
-                error: err,
-                response: data
-            };
-            res.status(400).send(JSON.strinify(msg));
-        } else {
+        if (!err) {
             var jobs = [];
 
             if (data && data.length > 0) {
@@ -53,16 +45,14 @@ self.getJobs = function (req, res) {
 self.getJobByID = function (req, res) {
     var id = req.params.id;
     ht.getJobs(id, function (err, data) {
-        if (err) {
-            var msg = {
-                status: 'fail',
-                message: err.error,
-                error: err,
-                response: data
-            };
-            res.status(404).send(JSON.strinify(msg));
-        } else {
+        if (!err) {
             var job = data[0];
+
+            var aboutCsgPro = '<h3>About CSG PRO</h3>' + "\n\r";
+            aboutCsgPro = aboutCsgPro + 'CSG Pro is a boutique consulting firm that has been providing custom business solutions for clients for over 20 years. We are a privately held, profitable firm that has grown up in Portland, serving mostly local clients. We are a Microsoft Gold Certified Partner specializing in BI/Analytics, Application Development, Windows Azure and SharePoint. We love what we do, and we have a lot of fun doing it.' + "\n\r\n\r";
+            aboutCsgPro = aboutCsgPro + 'Located in the vibrant Pearl District, we fully enjoy being in the cultural heart of the city. The neighborhood is bike friendly with great food, shops, galleries and all the best coffee shops. We look forward to hearing from you.';
+
+            job.Markdown = job.Markdown + aboutCsgPro;
 
             res.render('post', {
                 post: job,
