544d4b682891088338f326352c14a5672a103f3b
Ken Howard <ken@simplicatedweb.com>
2016-02-11 12:05:31 -0800

    fix(migration): Changed topics unique format

--- migrations/20160210212342-add-unique-topic-and-slug-to-topics.ts
+++ migrations/20160210212342-add-unique-topic-and-slug-to-topics.ts
@@ -5,7 +5,7 @@ import { sequelize, sqlAttribute } from '../src/database';
 
 export = {
     up: (queryInterface: Sequelize.QueryInterface, DataTypes: Sequelize.DataTypes): any => {
-        return sequelize.query(`ALTER TABLE topics ADD UNIQUE (${sqlAttribute('topic')}), ADD UNIQUE (${sqlAttribute('slug')})`);
+        return sequelize.query(`ALTER TABLE topics ADD UNIQUE (${sqlAttribute('topic')}, ${sqlAttribute('slug')})`);
     },
     down: (queryInterface: Sequelize.QueryInterface, DataTypes: Sequelize.DataTypes) => {
         // Restore backup.
