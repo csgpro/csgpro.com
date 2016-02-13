473a15b682132cb33ae3e673c7b9ef9ab15978a1
Ken Howard <ken@simplicatedweb.com>
2016-02-13 12:20:42 -0800

    refactor(models): Improve Type definitions. Include virtual instance method definitions.

--- src/models/topic.model.ts
+++ src/models/topic.model.ts
@@ -1,41 +1,33 @@
 import * as Sequelize from 'sequelize';
 import { sequelize } from '../database';
-import { Post, IPostModel } from './post.model';
+import { Post, IPostInstance, IPostAttributes } from './post.model';
 
-interface ITopicSchema extends Sequelize.DefineAttributes {
-    topic: Sequelize.DefineAttributeColumnOptions;
-    slug: Sequelize.DefineAttributeColumnOptions;
-}
-
-interface ITopicModelOptions {
-}
-
-export interface ITopicModel extends ITopicModelOptions {
+export interface ITopicAttributes {
     id: number,
     topic: string;
     slug: string;
-    posts: IPostModel[]
+    posts?: IPostInstance[]
 }
 
-export interface TopicInstance extends Sequelize.Instance<ITopicSchema>, ITopicModel { };
+export interface ITopicInstance extends Sequelize.Instance<ITopicAttributes> {
+    getPosts: Sequelize.HasManyGetAssociationsMixin<IPostInstance>;
+    setPosts: Sequelize.HasManySetAssociationsMixin<IPostInstance, number>;
+    addPosts: Sequelize.HasManyAddAssociationsMixin<IPostInstance, number>;
+    addPost: Sequelize.HasManyAddAssociationMixin<IPostInstance, number>;
+    createPost: Sequelize.HasManyCreateAssociationMixin<IPostAttributes>;
+    removePost: Sequelize.HasManyRemoveAssociationMixin<IPostInstance, number>;
+    hasPost: Sequelize.HasManyHasAssociationMixin<IPostInstance, number>;
+    hasPosts: Sequelize.HasManyHasAssociationsMixin<IPostInstance, number>;
+    countPosts: Sequelize.HasManyCountAssociationsMixin;
+};
 
-var TopicSchema: ITopicSchema = {
+let TopicSchema: Sequelize.DefineAttributes = {
     topic: { type: Sequelize.STRING, unique: true, allowNull: false },
     slug: { type: Sequelize.STRING, unique: true, allowNull: false }
 };
 
-var TopicSchemaOptions: Sequelize.DefineOptions<TopicInstance> = {
-    timestamps: false,
-    instanceMethods: {},
-    hooks: {
-        beforeFind: function postsBeforeFind(options:any, fn:any) {
-            let postAttributes: any = ['title', 'slug'];
-            options.include = [
-                { model: Post, through: { attributes: [] }, attributes: postAttributes },
-            ];
-            fn(null, options);
-        }
-    }
+let TopicSchemaOptions: Sequelize.DefineOptions<ITopicInstance> = {
+    timestamps: false
 };
 
-export var Topic: Sequelize.Model<TopicInstance, any> = sequelize.define('topic', TopicSchema, TopicSchemaOptions);
+export let Topic = sequelize.define<ITopicInstance, ITopicAttributes> ('topic', TopicSchema, TopicSchemaOptions);
--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -3,8 +3,10 @@
 import * as hapi from 'hapi';
 import * as boom from 'boom';
 import * as Sequelize from 'sequelize';
-import { Post, IPostModel } from '../models/post.model';
+import { User } from '../models/user.model';
 import { Topic } from '../models/topic.model';
+import { Post } from '../models/post.model';
+import { PostCategory } from '../models/post-category.model';
 
 const routes: hapi.IRouteConfiguration[] = [
     {
@@ -17,6 +19,58 @@ const routes: hapi.IRouteConfiguration[] = [
                 reply(response);
             }, (err) => {
                 reply(boom.create(500, err));
+            });
+        }
+    },
+    {
+        method: 'GET',
+        path: '/{categorySlug}/{postSlug}',
+        handler: (request, reply) => {
+            let categorySlug = request.params['categorySlug'];
+            let postSlug = request.params['postSlug'];
+            PostCategory.findOne({ where: { slug: categorySlug } }).then(category => {
+                if (category) {
+                    category.getPosts({ where: { slug: postSlug } }).then(posts => {
+                        if (posts && posts[0]) {
+                            let post = posts[0].toJSON();
+                            return reply.view('home', { title: post.title, address: post.post });
+                        }
+                        return reply(boom.notFound());
+                    });
+                } else {
+                    reply.continue();
+                }
+            });
+        }
+    },
+    {
+        method: 'GET',
+        path: '/{categorySlug}',
+        handler: (request, reply) => {
+            let categorySlug = request.params['categorySlug'];
+            let includePosts: boolean = (request.query.includePosts && (request.query.includePosts === 'true')); // ?includePosts=true
+            PostCategory.findOne({ where: { slug: categorySlug } }).then(category => {
+                if (!category) {
+                    return reply(boom.notFound());
+                }
+                
+                let categoryJSON = { category: category.get('category'), slug: category.get('slug') };
+                
+                if (!includePosts) {
+                    reply({ category: categoryJSON });
+                } else {
+                    // Get Posts for topicSlug Where publishedDate > Jan 1, 1993
+                    // Include the post category.
+                    let postAttributes: any = ['title', 'slug', 'excerpt', 'publishedAt'];
+                    let getPostsWhere: Sequelize.WhereOptions = { publishedAt: { $gt: new Date('1993-01-01') } };
+                    let getPostsIncludes = [{ model: Topic, attributes: { exclude: ['id'] }, through: { attributes: <any>[] } }, { model: User, as: 'author', attributes: { exclude: ['id', 'username', 'createdAt', 'updatedAt', 'password', 'roleId'] } }];
+                    // exclude the 'postTopic' object by passing an empty array to `joinTableAttributes`
+                    let getPostsOptions: any = { where: getPostsWhere, attributes: postAttributes, joinTableAttributes: [], include: getPostsIncludes };
+                    
+                    category.getPosts(getPostsOptions).then(posts => {
+                        reply({ category: categoryJSON, postCount: posts.length, posts: posts });
+                    });
+                }
             });
         }
     }
