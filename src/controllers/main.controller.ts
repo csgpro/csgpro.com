442f3ab34882167cfb4e073faa764a26172bba9b
Ken Howard <ken@simplicatedweb.com>
2016-04-20 19:27:45 -0700

    refactor(layout): Add favicon, update page titles

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -5,5 +5,5 @@ import * as boom from 'boom';
 
 index.sitemap = true;
 export function index(request: hapi.Request, reply: hapi.IReply) {
-    reply.view('home', { title: 'Home', description: '', address: 'CSG Pro<br>Portland, Oregon' }, { layout: 'hero-layout' });
+    reply.view('home', { title: 'Application Development, Business Analytics, and Cloud Services', description: '', address: 'CSG Pro<br>Portland, Oregon' }, { layout: 'hero-layout' });
 }
