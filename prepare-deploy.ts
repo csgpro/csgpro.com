a673d04a118a4234c37b347d2620e0724ac32d4b
Ken Howard <ken@simplicatedweb.com>
2016-05-01 15:07:43 -0700

    Prepare deploy

---
+++ prepare-deploy.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+import * as fs from 'fs';
+
+let ignoreFile = fs.readFileSync('.gitignore');
+
+let content = ignoreFile.toString().replace(/\*\.[css|js]{2,3}\s/g, (match) => {
+    return `# ${match}`;
+});
+
+content += '\n*.ts'; // Ignore TypeScript files
+
+console.log('Updating .gitignore', '\n', content);
+
+fs.writeFile('.gitignore', content, 'utf8', err => {
+    if (err) return console.log(err);
+});
