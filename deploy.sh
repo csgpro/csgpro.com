ecbbd9c0f04de0085a8a4bd35cd5deb049d19c9a
Ken Howard <ken@simplicatedweb.com>
2016-05-01 12:42:11 -0700

    deploy. blah

--- deploy.sh
+++ deploy.sh
@@ -105,11 +105,14 @@ selectNodeVersion
 
 # 2. Install npm packages
 if [ -e "$DEPLOYMENT_SOURCE/package.json" ]; then
+  echo Installing dependencies
+  cd "$DEPLOYMENT_SOURCE"
   eval $NPM_CMD install
   exitWithMessageOnError "npm failed"
 fi
 
 # 3. KuduSync to Target
+echo Copying source to target
 "$KUDU_SYNC_CMD" -v 500 -f "$DEPLOYMENT_SOURCE" -t "$DEPLOYMENT_TARGET" -n "$NEXT_MANIFEST_PATH" -p "$PREVIOUS_MANIFEST_PATH" -i ".git;.hg;.deployment;deploy.sh"
   exitWithMessageOnError "Kudu Sync to Target failed"
   
