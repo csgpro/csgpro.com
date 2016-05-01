5519a417e2c621aef7794edc055f93ac7a07457a
Ken Howard <ken@simplicatedweb.com>
2016-04-30 22:56:53 -0700

    refactor(careers): Update to use pagination

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -61,7 +61,7 @@ let PostSchemaOptions: Sequelize.DefineOptions<IPostInstance> = {
             let postMonth = ("0" + (self.getDataValue('publishedAt').getMonth() + 1)).slice(-2);
 
             if (categorySlug === 'career') {
-                permalink = `/careers/${postSlug}`;
+                permalink = `/careers/${postYear}/${postMonth}/${postSlug}`;
             } else if (categorySlug === 'blog') {
                 permalink = `/blog/${postYear}/${postMonth}/${postSlug}`;
             } else if (categorySlug === 'news') {
