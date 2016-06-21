32f6c8111a54d932079477c6270777b76f1896d9
Ken Howard <ken@simplicatedweb.com>
2016-06-21 15:59:46 -0700

    refactor(Business Analytics): Updated content

--- src/controllers/business-analytics.controller.ts
+++ src/controllers/business-analytics.controller.ts
@@ -6,35 +6,17 @@ import { pageView, pageHeader } from '../modules/view-matcher';
 import { getPostsByTopic } from '../commands/post.commands';
 
 const pageContent = `
-<p class="paragraph-primary">We are specialists with data. We help you make better decisions that support your business strategy. Those decisions happen by transforming and analyzing data into actionable information. This is what we do and care about, every day. The outcome &mdash; delighted clients.</p>
+<p class="paragraph-primary">CSG is a successful business analytics company and specialists with data.</p>
+ 
+<p class="paragraph-primary">We give decision-makers and other stakeholders access to data to make better decisions that support your business strategy.   Many times we reveal previously unavailable, unknown, and untapped information on, for example, customers, trends, and demands.  How?   We integrate data from multiple incompatible systems into a form that provides a single, consistent, subject-oriented view of the organization, transforming and analyzing this data into meaningful, actionable  information.</p>
 
-#### What is CSG Pro good at?
-
-- We're people oriented and data driven. We are quick-thinking
-when it comes to understanding your business and your data.
-
-- We specialize in solving problems using Microsoft data
-technologies, and Open Source and Microsoft development
-frameworks and tools.
-
-- When it comes to technology, we're good at choosing leading
-edge winners with a high value and low cost for our clients.
-Microsoft Power BI is a prime example. We were early adopters and
-have become Microsoft endorsed, respected experts in the region.
-
-#### What separates CSG Pro from our competitors?
-
-- We listen. Our experience, expertise and commitment reduce risk
-and ensure your project succeeds. We're comfortable working at
-all levels of an organization, and are highly productive for you with
-this competence.
-
-- We're easy to work with. We have a long term local commitment.
-The importance of our relationship can’t be understated. We work
-hard to earn your trust.
-
-- Your success is our measure of success, so we won't let you fail. We
-do what it takes to deliver what we promise, every time.
+#### What we are good at.
+ 
+We're people-oriented and data-driven. We are quick thinking when it comes to understanding your business and your data.  When it comes to technology, we're good at choosing leading edge winners with a high value and low cost for our clients.  Microsoft Power BI is a prime example.  We were early adopters and have become Microsoft endorsed, respected experts in the region.   This is what we do and care about, every day.
+ 
+#### What separates us from our competitors?
+ 
+We listen. Our experience, expertise and commitment reduce risk and ensure your project succeeds. We’re comfortable working at all levels of an organization, and are highly productive for you with this competence. We're easy to work with.  We have a long term local commitment. The importance of our relationships can't be understated.  Your success is our measure of success, so we won't let you fail.  The result – delighted clients.
 `;
 
 const powerBISamples = [
