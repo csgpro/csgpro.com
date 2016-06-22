ed6e168a519b460d951340939e437f213b202011
Ken Howard <ken@simplicatedweb.com>
2016-06-21 17:14:59 -0700

    feat(Social Links): Added social links. Default all external links to new window.

--- src/browser-scripts/main.ts
+++ src/browser-scripts/main.ts
@@ -4,6 +4,7 @@ import * as $ from 'jquery';
 require('./hero');
 require('./contact-form');
 require('./download-form');
+require('./external-links');
 require('./footer');
 
 $(document).foundation();
