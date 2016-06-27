f45f16c31a93c0f61bb99f3b589f2d40f3cc2963
Ken Howard <ken@simplicatedweb.com>
2016-06-26 23:52:14 -0700

    refactor(topnav): Update links to use routerLinkActive directive

--- src/admin/components/header/header.component.ts
+++ src/admin/components/header/header.component.ts
@@ -1,7 +1,3 @@
-// angular
-import {OnInit, OnDestroy} from '@angular/core';
-import {Router, NavigationEnd} from '@angular/router';
-
 // framework
 import {BaseComponent} from '../../framework';
 
@@ -13,27 +9,10 @@ import AuthService from '../../services/authentication.service';
     templateUrl: 'header.html',
     styleUrls: ['header.scss']
 })
-export class HeaderComponent implements OnInit, OnDestroy {
-
-    activeUrl: string;
-
-    private sub: any;
-
-    constructor(private _router: Router, private _auth: AuthService) { }
+export class HeaderComponent {
+    constructor(private _auth: AuthService) { }
 
     logout() {
         this._auth.logout();
-    }
-
-    ngOnInit() {
-        this.sub = this._router.events.subscribe((event) => {
-            if (event instanceof NavigationEnd) {
-                this.activeUrl = event.url;
-            }
-        });
-    }
-
-    ngOnDestroy() {
-        this.sub.unsubscribe();
     }
 }
