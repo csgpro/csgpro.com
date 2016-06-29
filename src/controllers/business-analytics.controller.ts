ff4dd5d7b4ea19c2b1541084ac82c7f0a825980a
Ken Howard <ken@simplicatedweb.com>
2016-06-29 09:40:07 -0700

    Update Business Analytics content

--- src/controllers/business-analytics.controller.ts
+++ src/controllers/business-analytics.controller.ts
@@ -6,17 +6,22 @@ import { pageView, pageHeader } from '../modules/view-matcher';
 import { getPostsByTopic } from '../commands/post.commands';
 
 const pageContent = `
-<p class="paragraph-primary">CSG is a successful business analytics company and specialists with data.</p>
- 
-<p class="paragraph-primary">We give decision-makers and other stakeholders access to data to make better decisions that support your business strategy.   Many times we reveal previously unavailable, unknown, and untapped information on, for example, customers, trends, and demands.  How?   We integrate data from multiple incompatible systems into a form that provides a single, consistent, subject-oriented view of the organization, transforming and analyzing this data into meaningful, actionable  information.</p>
+<p class="paragraph-primary"><strong>CSG Pro Business Analytics and Consulting Services</strong> - We love data and we love empowering our clients even more.</p>
+
+<p>We work with you to surface data so that you and your organization can make data-driven decisions. We routinely integrate data from multiple sources and seemingly incompatible systems, transforming raw data to produce meaningful and actionable information. Our purpose is to improve the quality and quantity of information available at your fingertips.
+</p>
 
 #### What we are good at.
  
-We're people-oriented and data-driven. We are quick thinking when it comes to understanding your business and your data.  When it comes to technology, we're good at choosing leading edge winners with a high value and low cost for our clients.  Microsoft Power BI is a prime example.  We were early adopters and have become Microsoft endorsed, respected experts in the region.   This is what we do and care about, every day.
+We're people-oriented and data savvy. We are quick to understand your business and the information that matters most.  When it comes to technology, we're good at choosing leading edge winners that deliver high value and low cost for our clients.  Microsoft Power BI is a prime example.  We were early adopters of Power BI and are the recognized experts in the region. Business Analytics is our daily focus and our passion.
  
 #### What separates us from our competitors?
  
-We listen. Our experience, expertise and commitment reduce risk and ensure your project succeeds. We’re comfortable working at all levels of an organization, and are highly productive for you with this competence. We're easy to work with.  We have a long term local commitment. The importance of our relationships can't be understated.  Your success is our measure of success, so we won't let you fail.  The result – delighted clients.
+We listen. Our experience, expertise and commitment reduce risk and ensure your project succeeds. We’re highly productive, easy to work with and competent engaging at all levels of your organization.  For us, the importance of our long-term relationships are paramount and your success is our first measure of success. We won’t let you fail.
+
+#### Thoughts, questions, comments.
+
+We'd like to get to know you and a little more of your unique situation.   Let's see if a fit exists.   Reach out <a data-open="contactModal">here</a> or give us a quick call at 503-292-0859.
 `;
 
 const powerBISamples = [
