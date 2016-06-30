20f64a1094ec61c46956fa780dabd0e801e40e68
Ken Howard <ken@simplicatedweb.com>
2016-06-30 11:49:35 -0700

    Add author dropdown.

--- src/admin/pages/posts/post.component.ts
+++ src/admin/pages/posts/post.component.ts
@@ -1,12 +1,17 @@
 // angular
 import {OnInit, OnDestroy} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {NgForm}    from '@angular/common';
+
+// libs
+import * as _ from 'lodash';
 
 // framework
 import {BaseComponent} from '../../framework';
 
 // app
 import {PostService, Post} from '../../models/post';
+import {UserService, User} from '../../models/user';
 import {LoadingService} from '../../services/loading.service';
 import {LoadingIndicatorComponent} from '../../components/loading-indicator/loading-indicator.component';
 import {MarkdownService} from '../../services/markdown.service';
@@ -20,17 +25,41 @@ export class PostComponent implements OnInit, OnDestroy {
 
     post = new Post();
 
+    // selects
+    authors: User[] = [];
+
     private _paramsSubscription: any;
 
-    constructor(private _postService: PostService, public loadingService: LoadingService, private _route: ActivatedRoute, public markdown: MarkdownService) {}
+    constructor(private _postService: PostService, private _userService: UserService, public loadingService: LoadingService, private _route: ActivatedRoute, public markdown: MarkdownService) {}
 
     ngOnInit() {
         // Get post
         this.loadingService.on();
         this._paramsSubscription = this._route.params.subscribe(params => {
             let postId = +params['id'];
-            this._postService.get(postId).then(post => {
+
+            let queue = [];
+
+            queue.push(this._postService.get(postId));
+            queue.push(this._userService.get());
+
+            Promise.all(queue).then(response => {
+                const [post, users] = response;
+
                 this.post = post;
+                this.authors = users;
+            })
+            .then(() => {
+                let currentAuthorIndex = _.findIndex(this.authors, { id: this.post.author.id });
+                if (currentAuthorIndex > -1) {
+                    this.post.author = this.authors[currentAuthorIndex];
+                }
+            })
+            .then(() => {
+                // done
+                this.loadingService.off();
+            }).catch(() => {
+                // done
                 this.loadingService.off();
             });
         });
