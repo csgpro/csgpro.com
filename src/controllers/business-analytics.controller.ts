fd015c751b8960db5a15008390ec208606478b81
Ken Howard <ken@simplicatedweb.com>
2016-07-07 14:26:01 -0700

    refactor(Content): Update static content

--- src/controllers/business-analytics.controller.ts
+++ src/controllers/business-analytics.controller.ts
@@ -6,22 +6,28 @@ import { pageView, pageHeader } from '../modules/view-matcher';
 import { getPostsByTopic } from '../commands/post.commands';
 
 const pageContent = `
-<p class="paragraph-primary"><strong>CSG Pro Business Analytics and Consulting Services</strong> - We love data and we love empowering our clients even more.</p>
-
-<p>We work with you to surface data so that you and your organization can make data-driven decisions. We routinely integrate data from multiple sources and seemingly incompatible systems, transforming raw data to produce meaningful and actionable information. Our purpose is to improve the quality and quantity of information available at your fingertips.
-</p>
+<p class="paragraph-primary">We love data and we love empowering our clients even more.  We work with you to surface data
+so that you and your organization can make data-driven decisions. We routinely integrate data from multiple sources and
+seemingly incompatible systems, transforming raw data to produce meaningful and actionable information. Our purpose is
+to improve the quality and quantity of information available at your fingertips.</p>
 
 #### What we are good at.
  
-We're people-oriented and data savvy. We are quick to understand your business and the information that matters most.  When it comes to technology, we're good at choosing leading edge winners that deliver high value and low cost for our clients.  Microsoft Power BI is a prime example.  We were early adopters of Power BI and are the recognized experts in the region. Business Analytics is our daily focus and our passion.
+We're people-oriented and data savvy. We are quick to understand your business and the information that matters most.
+When it comes to technology, we’re good at choosing leading edge winners that deliver high value and low cost for our clients.
+Microsoft Power BI is a prime example.  We were early adopters of Power BI and are the recognized experts in the region.
+Business Analytics is our daily focus and our passion.
  
 #### What separates us from our competitors?
  
-We listen. Our experience, expertise and commitment reduce risk and ensure your project succeeds. We’re highly productive, easy to work with and competent engaging at all levels of your organization.  For us, the importance of our long-term relationships are paramount and your success is our first measure of success. We won’t let you fail.
+We listen. Our experience, expertise and commitment reduce risk and ensure your project succeeds. We're highly productive,
+easy to work with and competent engaging at all levels of your organization.  For us, the importance of our long-term
+relationships are paramount and your success is our first measure of success. We won't let you fail.
 
 #### Thoughts, questions, comments.
 
-We'd like to get to know you and a little more of your unique situation.   Let's see if a fit exists.   Reach out <a data-open="contactModal">here</a> or give us a quick call at 503-292-0859.
+We'd like to get to know you and a little more of your unique situation. Let's see if a fit exists. Reach out <a data-open="contactModal">here</a>
+or give us a quick call at 503-292-0859.
 `;
 
 const powerBISamples = [
--- src/controllers/custom-software.controller.ts
+++ src/controllers/custom-software.controller.ts
@@ -6,23 +6,36 @@ import { pageView, pageHeader } from '../modules/view-matcher';
 import { getPostsByTopic } from '../commands/post.commands';
 
 const pageContent = `
-<p class="paragraph-primary">At CSG, our team combines Microsoft and Open Source technologies to create custom software solutions. People first decide to build a custom solution rather than buy because they have a strategic business need that when addressed through custom software adds competitive advantage or key cost reduction.  Often it adds an essential ingredient to the success of that business.   They build it to fit because they cannot buy it to fit complex or unique business requirements with out-of-the-box solutions.  CSG solutions are accessible at any time, from any device to support people on the go.</p>
+<p class="paragraph-primary">At CSG, our team combines Microsoft and Open Source technologies to create custom software solutions.
+Our clients find strategic benefits and a competitive advantage from our custom solutions that out-of-the-box solutions don't offer.
+Often a custom software solution adds an essential ingredient to the success of their business and reduces total cost of ownership.
+Utilizing the latest technologies, CSG solutions are accessible at any time, from any device, to support people on the go.</p>
 
 #### What we are good at.
 
-We're passionate about designing and building custom software solutions – web, mobile and line of business - that improve business processes with accurate and streamlined data collection, improve relationships with internal and external customers and by making your employees more productive.
+We're passionate about designing and building custom software solutions &mdash; web, mobile and line of business - that improve business
+processes with accurate and efficient data collection, improve relationships with internal and external customers and make your employees
+more productive.
 
-Already have a development team? We're happy to work together, combining highly productive members of our Custom Solutions Team with yours.  Whether they are highly skilled, just resource constrained, or whether they receive mentoring from CSG on the latest technologies, we've found blended teams an enormously successful way to develop complex, business-critical applications.
+Already have a development team? We're happy to work together, combining highly productive members of our Custom SolutionsTeam with yours.
+Whether your team is highly skilled and resource constrained, or they receive mentoring from CSG on the latest technologies,
+we've found blended teams an enormously successful way to develop complex, business-critical applications.
 
 #### What separates us from our competitors?
 
-Our people listen. We're experts in Software Development Lifecycle and Agile Development, placing a high importance on collaboration between the client and the project team.   
+Our people listen. We're experts in Software Development Lifecycle and Agile Development, placing a high importance on collaboration
+between the client and the project team.
 
-Our clients find the results compelling - we provide transparency, predictable costs and schedule, focus on end users and business value, and improved quality. Our commitment is delighting you.
+Our clients find the results compelling &mdash; we provide transparency, predictable costs and schedule, focus on end users and
+business value, and improved quality. Our commitment is delighting you.
+
+CSG's Custom Solutions Team is an active Microsoft Gold Application Development Partner.
+
 
 #### Thoughts, questions, comments.
 
-We'd like to get to know you and a little more of your unique situation.   Let's see if a fit exists.   Reach out <a data-open="contactModal">here</a> or give us a quick call at 503-292-0859.
+We'd like to get to know you and a little more of your unique situation. Let's see if a fit exists. Reach out <a data-open="contactModal">here</a>
+or give us a quick call at 503-292-0859.
 `;
 
 index.sitemap = true;
