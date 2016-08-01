49ee5861c68208af3ddbfdc9bf7149fdd56e859b
Ken Howard <ken@simplicatedweb.com>
2016-08-01 11:52:19 -0700

    fix(Adjust homepage title and description):
    Fixes #148

--- src/controllers/main.controller.ts
+++ src/controllers/main.controller.ts
@@ -5,5 +5,5 @@ import * as boom from 'boom';
 
 index.sitemap = true;
 export function index(request: hapi.Request, reply: hapi.IReply) {
-    reply.view('home', { title: 'Application Development, Business Analytics, and Cloud Services', description: '', address: 'CSG Pro<br>Portland, Oregon' }, { layout: 'hero-layout' });
+    reply.view('home', { title: 'Custom Software, Business Analytics, and Cloud Services', description: `Headquartered in Portland Oregon's urban, tech savvy Pearl District, CSG Pro is committed to delighting its clients through the innovative application of modern technology solutions. We hope to have the opportunity to build something amazing for you today, as we have for many since 1993.` }, { layout: 'hero-layout' });
 }
