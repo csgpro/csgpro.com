9fb3c1292c5ade15f6f5f09e80626df10f345c08
JamieMikami <jamiem@csgpro.com>
2016-10-21 22:06:07 -0700

    Resolves #157 About Page Changes

--- server/controllers/about.controller.ts
+++ server/controllers/about.controller.ts
@@ -13,21 +13,6 @@ class AboutController implements Controller {
     baseUrl: string;
     routes: () => hapi.IRouteConfiguration[];
 
-    pageContent = `
-<p class="paragraph-primary">CSG Pro builds modern software solutions that expand knowledge through
-a more informed and visual use of data. Through Business Analytics and Custom Software Solutions, CSG Pro
-delivers value by helping clients intelligently bring together, analyze and visualize data to make informed
-decisions and streamline their business processes. We integrate cloud and mobile as appropriate to allow
-for scale, enhance the user’s experience and ensure long-term payoff.</p>
-
-CSG Pro maintains an active Microsoft Partner status with a Gold Application Development Competency.
-CSG Pro's commitment to learning is highlighted by active participation and leadership in monthly
-Power BI and Typescript meet ups.
-
-Headquartered in Portland Oregon's urban, tech savvy Pearl District, CSG Pro is committed to delighting its
-clients through the innovative application of modern technology solutions. We hope to have the opportunity
-to build something amazing for you today, as we have for many since 1993.
-    `;
 
     @get('/')
     @config({ plugins: { sitemap: { include: true } } })
@@ -37,8 +22,7 @@ to build something amazing for you today, as we have for many since 1993.
         {
             title: 'About CSG Pro',
             description: '',
-            header: headerImage,
-            pageContent: this.pageContent
+            header: headerImage
         },
         { layout: 'hero-layout' });
     }
