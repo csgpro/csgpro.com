e612b353711ecae46f24390a10c590f7f817ec45
Ken Howard <kenh@csgpro.com>
2016-10-27 05:21:46 -0700

    chore(admin): Delete error and success summary components

--- admin/components/index.ts
+++ admin/components/index.ts
@@ -1,4 +1,1 @@
-import { ErrorSummary }              from './error-summary/error-summary.component';
-import { SuccessSummary }            from './success-summary/success-summary.component';
-
-export const ComponentProvider = [ErrorSummary, SuccessSummary];
+export const ComponentProvider: any[] = [];
